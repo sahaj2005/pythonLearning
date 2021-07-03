63707db0770bbb0c053e07367ee04102cd459437
palwinder Singh <palwinder.singh@capgemini.com>
2021-07-03 09:14:21 +0530

    QR code

--- QRcode.py
+++ QRcode.py
@@ -1,10 +1,44 @@
 from tkinter import*
+from tkinter import messagebox
+import qrcode
+
 screen=Tk()
 screen.title('QR code')
 screen.maxsize(width=450,height=250)
 screen.minsize(width=465,height=250)
 screen.configure(bg='blue')
 
+def generate():
+   name= Name_entry_box.get()
+   id=ID_entery_box.get()
+   message=Message_entery_box.get()
+   detail='name:'+name+'\n'+'Id:'+id+'\n'+'message:'+message
+   img = qrcode.make(detail)
+   type(img)
+   img.save("QR code.png")
+
+
+
+
+
+
+
+
+
+
+
+
+def Quit_root():
+    res=messagebox.askokcancel('notification','Are you sure you want to quit')
+    if res==True:
+        screen.destroy()
+
+    else:
+        pass
+def clean():
+    Name_entry_box.delete(0,END)
+    ID_entery_box.delete(0,END)
+    Message_entery_box.delete(0,END)
 def on_leavegenerate(s):
     generate.configure(bg='pink')
 def on_entergenerate(s):
@@ -26,16 +60,16 @@ def on_enterQuit(s):
 QR_ID_label=Label(screen,text='Enter Your ID',font=('arial',13,'italic bold'),bg='powder blue',fg='red',width=17)
 QR_ID_label.place(x=20,y=40)
 
-QR_Name_label=Label(screen,text='Enter Your Message ',font=('arial',13,'italic bold'),bg='powder blue',fg='red',width=17)
+QR_Name_label=Label(screen,text='Enter Your Name ',font=('arial',13,'italic bold'),bg='powder blue',fg='red',width=17)
 QR_Name_label.place(x=20,y=80)
 
 QR_Message_label=Label(screen,text='Enter Your message',font=('arial',13,'italic bold'),bg='powder blue',fg='red',width=17)
 QR_Message_label.place(x=20,y=120)
 
-QR_notifaction_label=Label(text='notification :',font=('arial',13,'italic bold'),bg='powder blue',fg='red',width=17)
-QR_notifaction_label.place(x=20,y=220)
-notifaction_box=Label(screen,text='',font=('arial',13,'italic bold'),bg='pink',width=27)
-notifaction_box.place(x=210,y=220)
+#QR_notifaction_label=Label(text='notification :',font=('arial',13,'italic bold'),bg='powder blue',fg='red',width=17)
+#QR_notifaction_label.place(x=20,y=220)
+#notifaction_box=Label(screen,text=notification,font=('arial',13,'italic bold'),bg='pink',width=27,)
+#notifaction_box.place(x=210,y=220)
 
 ###################################
 ID_entery_box=Entry(screen,font=('arial',10,'italic bold'),bg='pink',bd=5,width=23)
@@ -47,19 +81,16 @@ Name_entry_box.place(x=280,y=80)
 Message_entery_box=Entry(screen,font=('arial',10,'italic bold'),bg='pink',bd=5,width=23)
 Message_entery_box.place(x=280,y=120)
 
-
-
-
 ########################
-generate=Button(screen,text='Generate',font=('arial',10,' bold'),bg='pink',bd=5,width=10,)
+generate=Button(screen,text='Generate',font=('arial',10,' bold'),bg='pink',bd=5,width=10,command=generate)
 generate.place(x=30,y=170)
 
 
-clean=Button(screen,text='clean',font=('arial',10,' bold'),bg='pink',bd=5,width=10,)
+clean=Button(screen,text='clean',font=('arial',10,' bold'),bg='pink',bd=5,width=10,command=clean)
 clean.place(x=190,y=170)
 
 
-Quit=Button(screen,text='Quit',font=('arial',10,' bold'),bg='pink',bd=5,width=10,)
+Quit=Button(screen,text='Quit',font=('arial',10,' bold'),bg='pink',bd=5,width=10,command=Quit_root)
 Quit.place(x=350,y=170)
 #######################################
 generate.bind('<Enter>',on_entergenerate)
